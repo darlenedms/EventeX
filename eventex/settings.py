7cb437044a7c092c318dd7c49dba1f77fb830c97
Darlene <darlene.santos@corp.globo.com>
2013-10-08 15:38:42 -0300

    Edit: Altera banco de dados para funcionar com mysql no heroku.

--- eventex/settings.py
+++ eventex/settings.py
@@ -1,6 +1,7 @@
 from unipath import Path
 PROJECT_DIR = Path(__file__).parent
 
+import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,16 +12,23 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',     # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': PROJECT_DIR.child('database.db'),   # Or path to database file if using sqlite3.
+#         'USER': '',                                 # Not used with sqlite3.
+#         'PASSWORD': '',                             # Not used with sqlite3.
+#         'HOST': '',                                 # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '',                                 # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',     # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_DIR.child('database.db'),   # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(
+            default='sqlite:///' + PROJECT_DIR.child('database.db')
+        )
 }
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -47,18 +55,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = PROJECT_DIR.child('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_DIR.child('public')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
