9e746646e69fb3d5c12b052ec1c4247b6f370059
Darlene <darlene.santos@corp.globo.com>
2013-10-08 11:55:39 -0300

    Edit: Modifica a página index utilizando arquivos estáticos.

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-from django.shortcuts import render_to_response
+from django.views.generic.simple import direct_to_template
 
 
 def homepage(request):
-    return render_to_response('index.html')
+    return direct_to_template(request, template='index.html')
