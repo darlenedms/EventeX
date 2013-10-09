1d6d9689a5567e11dcc64ef97c922ae0e801ae81
Darlene <darlene.santos@corp.globo.com>
2013-10-09 17:49:12 -0300

    Add: Nova app para receber as inscrições dos interessados no evento.

---
+++ eventex/subscriptions/tests.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from django.test import TestCase
+
+
+class SubscribeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
+    def test_get(self):
+        'GET /inscricao/ must return status code 200.'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Response should be a rendered template.'
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
---
+++ eventex/subscriptions/views.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+from django.views.generic.simple import direct_to_template
+
+
+def subscribe(request):
+    return direct_to_template(request, 'subscriptions/subscription_form.html')
