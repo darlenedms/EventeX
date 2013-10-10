dd0c3d53771526841c02bfdd44629129e889102e
Darlene <darlene.santos@corp.globo.com>
2013-10-10 19:39:43 -0300

    Edit: Implementa o formulário de inscrição no evento.

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,37 @@
 # coding: utf-8
+from django.http import HttpResponseRedirect
 from django.views.generic.simple import direct_to_template
-from .forms import SubscriptionForm
+from django.shortcuts import get_object_or_404
+from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
-    return direct_to_template(request, 'subscriptions/subscription_form.html',
+    if request.method == 'POST':
+        return create(request)
+    else:
+        return new(request)
+
+
+def new(request):
+    return direct_to_template(request,
+                              'subscriptions/subscription_form.html',
                               {'form': SubscriptionForm()})
+
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+
+    if not form.is_valid():
+        return direct_to_template(request,
+                                  'subscriptions/subscription_form.html',
+                                  {'form': form})
+    obj = form.save()
+    return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
+
+
+def success(request, pk):
+    subscription = get_object_or_404(Subscription, pk=pk)
+    return direct_to_template(request,
+                              'subscriptions/subscription_detail.html',
+                              {'subscription': subscription})
