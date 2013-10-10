4782c355938492b012b4a89378b2654830110387
Darlene <darlene.santos@corp.globo.com>
2013-10-10 12:03:41 -0300

    Edit: Inclui o SubscriptionForm no contexto da view subscribe que o template subscription_form utiliza.

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from django.views.generic.simple import direct_to_template
+from .forms import SubscriptionForm
 
 
 def subscribe(request):
-    return direct_to_template(request, 'subscriptions/subscription_form.html')
+    return direct_to_template(request, 'subscriptions/subscription_form.html',
+                              {'form': SubscriptionForm()})
